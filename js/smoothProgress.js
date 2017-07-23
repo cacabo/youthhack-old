b1f304afe483e307903b71aed248dcd9c7acd812
ccabo1 <ccabo@seas.upenn.edu>
2017-07-23 14:10:59 -0700

    add styles for partners

--- js/smoothProgress.js
+++ js/smoothProgress.js
@@ -2,8 +2,13 @@ $f1 = $("#fluid-1");
 $f2 = $("#fluid-2");
 $f3 = $("#fluid-3");
 
+const interval = 1000;
+
 setInterval(function() {
   let r1 = Math.floor(Math.random() * 70);
-  console.log(rand);
+  let r2 = Math.floor(Math.random() * 50);
+  let r3 = Math.floor(Math.random() * 40);
   $f1.css('width', `${r1 + '%'}`);
-}, 1000);
+  $f2.delay(interval / 3).css('width', `${r2 + '%'}`);
+  $f3.delay(interval / 3 * 2).css('width', `${r3 + '%'}`);
+}, interval);
