9d66928a5eb8ab9709394dd97ced1dcdb1456db8
ccabo1 <ccabo@seas.upenn.edu>
2017-07-23 15:42:58 -0700

    finished contact form

--- js/formspree.js
+++ js/formspree.js
@@ -7,14 +7,14 @@ $contactForm.submit(function(e) {
         data: $(this).serialize(),
         dataType: 'json',
         beforeSend: function() {
-            $('#errors').html('<div class="alert alert-info">Sending message…</div>');
+            $('#errors').html('<div class="alert alert-info">Sending message…</div><div class="space-2"></div>');
         },
         success: function(data) {
-            $('#errors').html('<div class="alert alert-success">Message sent!</div>');
+            $('#errors').html('<div class="alert alert-success">Message sent!</div><div class="space-2"></div>');
             $contactForm.slideUp(200);
         },
         error: function(err) {
-            $('#errors').html('<div class="alert alert-warning">Oops! There was an error. Check the form and try again.</div>');
+            $('#errors').html('<div class="alert alert-warning">Oops! There was an error. Check the form and try again.</div><div class="space-2"></div>');
         }
     });
 });
