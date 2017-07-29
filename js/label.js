d05f37622f7efb35cb3b58917dd779ed4b538ebd
ccabo1 <ccabo@seas.upenn.edu>
2017-07-28 20:12:58 -0700

    updated form styling slightly

--- js/label.js
+++ js/label.js
@@ -1,31 +1,35 @@
 $(document).ready(function() {
 
-    'use strict';
+  'use strict';
 
-    // Apply the transform effect on focus
-    $('input').focus(function() {
-        $(this).next('label').addClass('hasVal');
-    });
+  // Apply the transform effect on focus
+  $('input').focus(function() {
+    $(this).addClass('active');
+    $(this).next('label').addClass('hasVal active');
+  });
 
-    $('textarea').focus(function() {
-        $('#messageLabel').addClass('hasVal');
-    });
+  $('textarea').focus(function() {
+    $(this).addClass('active');
+    $('#messageLabel').addClass('hasVal active');
+  });
 
-    // check if the input has a value
-    $('input').blur(function() {
-        if ($(this).val().length > 0) {
-            $(this).next('label').addClass('hasVal');
-        } else {
-            $(this).next('label').removeClass('hasVal');
-        }
-    });
+  // check if the input has a value
+  $('input').blur(function() {
+    $(this).removeClass('active');
+    if ($(this).val().length > 0) {
+      $(this).next('label').addClass('hasVal').removeClass('active');
+    } else {
+      $(this).next('label').removeClass('hasVal active');
+    }
+  });
 
-    $('textarea').blur(function() {
-        if ($(this).val().length > 0) {
-            $('#messageLabel').addClass('hasVal');
-        } else {
-            $('#messageLabel').removeClass('hasVal');
-        }
-    });
+  $('textarea').blur(function() {
+    $(this).removeClass('active');
+    if ($(this).val().length > 0) {
+      $('#messageLabel').addClass('hasVal').removeClass('active');
+    } else {
+      $('#messageLabel').removeClass('hasVal active');
+    }
+  });
 
 });
